97582aafee495dc82c25fe5e871df50c8bbce316
Your Name <juan@MacBook-Pro-de-Juan.local>
2018-04-19 21:34:54 -0500

    RestServer - listo

--- config/config.js
+++ config/config.js
@@ -10,8 +10,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //base de datos
 let urlDB;
 
-
-urlDB = 'mongodb://bc-user:guirova18@ds151809.mlab.com:51809/bailaconmigodb'
-
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/bailaConmigoDB';
+} else {
+    urlDB = 'mongodb://bc-user:guirova18@ds151809.mlab.com:51809/bailaconmigodb'
+}
 
 process.env.URLDB = urlDB;
