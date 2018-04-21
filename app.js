d807495bbe99aab09dd9b1dd86d6bc392c5ad671
Your Name <juan@MacBook-Pro-de-Juan.local>
2018-04-21 14:27:28 -0500

    editar

--- app.js
+++ app.js
@@ -18,14 +18,12 @@ var puntuacionRoutes = require('./routes/puntuacion');
 
 //Inicializar variables
 var app = express();
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 
 // Body Parser
 // parse application/x-www-form-urlencoded
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 
 
 //Conexion a la base de datos
