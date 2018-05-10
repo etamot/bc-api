54a4f3b19224c2899900a562043420e3ab4b797f
Your Name <juan@MacBook-Pro-de-Juan.local>
2018-05-10 07:23:44 -0500

    cors

--- app.js
+++ app.js
@@ -7,6 +7,35 @@ var bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
 
+
+
+//Inicializar variables
+var app = express();
+
+
+
+//CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+});
+
+
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true, parameterLimit: 100000 }));
+
+
+
+
+
+
+
+
+
 // Importar Rutas
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
@@ -16,16 +45,6 @@ var rutinaRoutes = require('./routes/rutina');
 var corporacionRoutes = require('./routes/corporacion');
 var puntuacionRoutes = require('./routes/puntuacion');
 var busquedaRutina = require('./routes/busquedaRutina');
-
-//Inicializar variables
-var app = express();
-
-
-// Body Parser
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.json({ limit: '100mb' }));
-app.use(bodyParser.urlencoded({ limit: '100mb', extended: true, parameterLimit: 100000 }));
-
 
 //Conexion a la base de datos
 mongoose.connection.openUri(process.env.URLDB, (err, res) => {
